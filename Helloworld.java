0cc6ec3f1e56eb5b83b157915ba3afce5a669ad7
NithinSai11421 <153480138+NithinSai11421@users.noreply.github.com>
2023-12-11 10:37:53 +0530

    print helloworld

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+// java programming code for printing Hello world 
+public class HelloWorld
+{
+  public static void main(String args[])
+    {
+      System.out.println("Hello World!");
+      System.out.print("Hello Again.");
+    }
+}
